606b52dcd01ec41a53f7878fa093a61e068717be
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-01 15:16:32 -0400

    Moved Class to Separate File
    Moved the contents by using the shortcut {CTRL} {DOT} to move the class to a new file named Person.cs.  You can see this in the Solutions Eplorer.  If Solutions Eplorer is not Open {CTRL} + ALT + L.
    
    Note to Open the File you can also click in the word "Person" where it says new Person and press {F12}.  This will open the class in a popout window.
    
    To keep the Window Open {CTRL} + {ALT} + {HOME}.  When you do this the Person Tab will move from the top right to the top left.

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,45 +1,5 @@
 ﻿namespace CSharpFundamentals
 {
-    public class Person
-    {
-        /// <summary>
-        /// This is a field!
-        /// The purpose of this field is to store the users name
-        /// Later on I will cover properties which are more efficient.
-        /// </summary>
-        public string Name;
-
-        /// <summary>
-        /// This is a Method used to intruduce to people.
-        /// </summary>
-        /// <param name="to"></param>
-        public void Introduce(string to)
-        {
-            // Introductions are in order
-            System.Console.WriteLine("Hello {0}, I am {1}!", to, Name);
-        }
-
-        /// <summary>
-        /// This Function allows us to parse a string and return
-        /// an instance of the Person Class.  By using the static
-        /// keyword this function is available from the Class and
-        /// not an instance of the class.  An instance is considered
-        /// to be an object and you cannot call static functions from.
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static Person Parse(string str)
-        {
-            //  Lets instantiate a new object of the type Person
-            var person = new Person
-            {
-                Name = str
-            };
-
-            // Return the person object
-            return person;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
