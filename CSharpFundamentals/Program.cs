0b5f3d6469546d7c24c9c9d896854c8d0cbdf5fb
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-01 14:51:05 -0400

    Created Person Class
    Created the Person Class with a public Field and a couple methods.

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,9 +1,29 @@
 ﻿namespace CSharpFundamentals
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            System.Console.WriteLine("Hello {0}, I am {1}!", to, Name);
+        }
+
+        public Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var person = new Person();
+            person.Name = "Thing 1";
+            person.Introduce("Thing 2");            
         }
     }
 }
