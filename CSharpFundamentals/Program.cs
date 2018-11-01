571067c9c55c8733ada09e2ce65705cda92f1fbd
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-01 15:07:24 -0400

    Added Comments
    Commented Code in Main

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -44,14 +44,18 @@
     {
         static void Main(string[] args)
         {
+            //  New instance of the person class for Thing 1
             var person = new Person
             {
                 Name = "Thing 1"
             };
-
+            //  Introduce Thing 1 to Thing 2
             person.Introduce("Thing 2");
 
+            // Use Static Parse Method to create a new instance of
+            // the person class.
             var person2 = Person.Parse("King Thing");
+            //  Introduce King Thing to Super Thing
             person2.Introduce("Super Thing");
         }
     }
