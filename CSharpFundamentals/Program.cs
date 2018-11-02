8957a45b65c04a864c7b2059440890390aa3a091
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-02 13:28:42 -0400

    Added Comments to Code
    It is a best practice to comment your code as you go... Not as I have done here where I commented code after the fact.  But within a time frame that I would remember clearly what was going on.  Do not code the whole way through without commenting.  Commenting your code after the fact is never as effective as when you are there.  Additionally reviewing your comments after you have taken your eyes off sometimes can be helpful.

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -35,21 +35,25 @@ namespace CSharpFundamentals
 
         private static void ClassConstructorPartI()
         {
+            // Using the Class Constructor with the signature of type ID and the Object Initilizer to set the Field Name
             var customer = new Customer(1) { Name = "Thing One" };
             System.Console.WriteLine("Searching for customer id:  {0} with the name of {1}", customer.Id, customer.Name);
 
+            //  Using the Class constructor looking for the ID of type int
             var customer2 = new Customer(2);
+            //  Then set the value of that customer2 Name Field
             customer2.Name = "Thing King";
             System.Console.WriteLine("Searching for customer id:  {0} with the name of {1}", customer2.Id, customer2.Name);
 
+            //  Using the Class Constructor to set both the ID and Name
             var customer3 = new Customer(3, "Thing Super");
             System.Console.WriteLine("Searching for customer id:  {0} with the name of {1}", customer3.Id, customer3.Name);
 
+            // Using Object Initializer to set both the Id and Name fields
             var customer4 = new Customer()
             {
                 Id = 4
-                ,
-                Name = "Chocolate Thunda"
+                , Name = "Chocolate Thunda"
             };
             System.Console.WriteLine("Searching for customer id:  {0} with the name of {1}", customer4.Id, customer4.Name);
         }
