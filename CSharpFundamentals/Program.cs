c8f3d10b72adcedbf65c57831e2bf2412eecd1e3
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-02 12:19:17 -0400

    Moved Method to Region
    Now I have moved the code into the Section 1... region.

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -10,6 +10,26 @@ namespace CSharpFundamentals
             ClassConstructors();
         }
 
+
+
+        #region Section 1 -- Introduction to Classes
+        private static void IntroductionToClasses()
+        {
+            //  New instance of the person class for Thing 1
+            var person = new Person
+            {
+                Name = "Thing One"
+            };
+            //  Introduce Thing 1 to Thing 2
+            person.Introduce("Thing Two");
+
+            // Use Static Parse Method to create a new instance of
+            // the person class.
+            var person2 = Person.Parse("King Thing");
+            //  Introduce King Thing to Super Thing
+            person2.Introduce("Super Thing");
+        }
+
         private static void ClassConstructors()
         {
             var customer = new Customer(1) { Name = "Thing One" };
@@ -29,24 +49,6 @@ namespace CSharpFundamentals
                 Name = "Chocolate Thunda"
             };
             System.Console.WriteLine("Searching for customer id:  {0} with the name of {1}", customer4.Id, customer4.Name);
-        }
-
-        #region Section 1 -- Introduction to Classes
-        private static void IntroductionToClasses()
-        {
-            //  New instance of the person class for Thing 1
-            var person = new Person
-            {
-                Name = "Thing One"
-            };
-            //  Introduce Thing 1 to Thing 2
-            person.Introduce("Thing Two");
-
-            // Use Static Parse Method to create a new instance of
-            // the person class.
-            var person2 = Person.Parse("King Thing");
-            //  Introduce King Thing to Super Thing
-            person2.Introduce("Super Thing");
         }
         #endregion
     }
