15883b7ba83809776337b85337e83c45c25e4c64
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-02 12:07:18 -0400

    Add Overrides to class
    Now we have 3 class constructors.  The Default (), one with type int and another with expecting an int and string.  Notcie each of these have their own constructos.

--- CSLib/Customers.cs
+++ CSLib/Customers.cs
@@ -5,9 +5,20 @@
         public int Id;
         public string Name;
 
+        public Customer()
+        {
+
+        }
+
         public Customer(int id)
         {
             this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
         }
     }
 }
