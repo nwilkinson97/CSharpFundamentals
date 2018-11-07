7760a4e5ab75bb9bdc9af0a0a8e2df6f396edf1b
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-07 15:20:30 -0500

    Create New Dictionary
    Dictionaries use Indexor's to store information temporarily in memory and then quickly access objects inside.

--- CSLib/HttpCookie.cs
+++ CSLib/HttpCookie.cs
@@ -1,6 +1,14 @@
-﻿namespace CSLib
+﻿using System.Collections.Generic;
+
+namespace CSLib
 {
     class HttpCookie
     {
+        private Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();        
+        }
     }
 }
