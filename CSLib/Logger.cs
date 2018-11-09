1fb0cebe8bdd3467f106b2e5f4d90a5b6497363b
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-09 11:34:13 -0500

    Created 3 Classes
    Created the DBMigrator, Installer, and Logger classes.

---
+++ CSLib/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSLib
+{
+    public class Logger
+    {
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
