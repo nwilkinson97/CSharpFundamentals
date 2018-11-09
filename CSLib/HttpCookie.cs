f753293c5df4ef4282745dacf793a921e869029c
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-09 10:12:17 -0500

    Create HTTP Coockie Class
    Created an HTTP Cookie Class

--- CSLib/HttpCookie.cs
+++ CSLib/HttpCookie.cs
@@ -4,11 +4,23 @@ namespace CSLib
 {
     class HttpCookie
     {
-        private Dictionary<string, string> _dictionary;
+        private readonly Dictionary<string, string> _dictionary;
 
         public HttpCookie()
         {
             _dictionary = new Dictionary<string, string>();        
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return _dictionary[key];
+            }
+            set
+            {
+                _dictionary[key] = value;
+            }
         }
     }
 }
