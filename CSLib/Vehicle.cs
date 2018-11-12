01e38a84c37bc2e048712b9da3103ba5bac7d2b5
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-11-12 12:16:13 -0500

    Create Vehicle Constructor with Parameter
    Here we initialized a private field for registrationNumber and update it thorugh the constructor.

--- CSLib/Vehicle.cs
+++ CSLib/Vehicle.cs
@@ -2,9 +2,16 @@
 {
     public class Vehicle
     {
+        private readonly string _registrationNumber;
+
         public Vehicle()
         {
             System.Console.WriteLine("Vehicle is being initialized.");
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            this._registrationNumber = registrationNumber;
         }
     }
 }
